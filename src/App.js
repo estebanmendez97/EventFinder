bd49fe5ed34244eb12b3a5b76e772a2eed1dbd5a
belyndisima82 <belyndisima@googlemail.com>
2018-09-04 15:17:02 -0500

    event API call

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import API_KEY from './Api.js';
 import Form from './components/form.jsx';
 import Events from './components/events.jsx';
 import Map from './components/map.jsx';
+import axius from 'axius';
 
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      eventList: [],
+      isLoades: false
+    }
+  }
 
-  getEvent = async () => {
-    const api_call = await fetch(`http://api.eventful.com/json/events/search?app_key=${API_KEY}&where=38.9072,-77.0369&within=5`);
+  componentWillMount() {
+    this.getEvent;
+  }
+
+  getEvent = async (e) => {
+    e.preventDefault();
+    const api_call = axius.get(`http://api.eventful.com/json/events/search?app_key=${API_KEY}&where=38.9072,-77.0369&within=5`);
     const data = await api_call.json();
     console.log(data);
   }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Form extends Component {
   render() {
     return (
-      <form>
+      <form onSubmit={this.props.getEvent}>
         <input type='text' name='category' placeholder='choose a category'/>
         <button>Get Events</button>
       </form>
